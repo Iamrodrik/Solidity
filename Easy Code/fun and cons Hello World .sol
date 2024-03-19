b913f272a9d50f6e0542ad444f96997eb31972fe
inme <105620385+Iamrodrik@users.noreply.github.com>
2024-03-19 13:58:43 +0600

    Update fun and cons Hello World .sol

--- Easy Code/fun and cons Hello World .sol
+++ Easy Code/fun and cons Hello World .sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+contract HelloWorld{
+    string public value;
+
+    constructor(){
+        value = "Hello, World!";
+    }
+    function getGreet() public view returns (string memory){
+        return value;
+    }
+    function setGreet(string memory _value) public {
+        value = _value;
+    }
+}
