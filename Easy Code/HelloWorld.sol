147d09d1504072aab6ab1a285e6818f37fd4104f
inme <105620385+Iamrodrik@users.noreply.github.com>
2024-03-19 13:36:27 +0600

    Update HelloWorld.sol
    add function to display hello world

--- Easy Code/HelloWorld.sol
+++ Easy Code/HelloWorld.sol
@@ -4,3 +4,13 @@ pragma solidity ^0.8.0;
 contract HelloWorld{
     string public message = "Hello, World!";
 }
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract HelloWorld{
+    function message() public pure returns (string memory){
+        return "Hello,World!";
+    }
+}
